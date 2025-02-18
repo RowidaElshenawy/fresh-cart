58c4d855855c892e0bc47825d83e4fdff29d8e6d
RowidaElshenawy <rowidaalielshenawy@gmail.com>
2025-02-19 01:46:00 +0200

    first commit

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -7,6 +7,7 @@ import Item from '../Shared/Item/Item'
 export default function Brands() {
     const[count,setCount]=useState(0)
     const[brands,SetBrands]=useState([])
+    const [ModalOpen, setModalOpen] = useState(null);
     async function getBrands(){
       try{
         let {data}=await axios.get('https://ecommerce.routemisr.com/api/v1/brands')
@@ -42,21 +43,19 @@ export default function Brands() {
 
 <h2 className='text-center pb-5 text-3xl font-semibold text-main pt-10'>All Brands</h2>
 <div className='flex flex-wrap justify-center items-center mb-10 '>
-  {brands.map(brand=>{
-    return<>
-    {/* Modal toggle */}
-    <button data-modal-target={brand._id} data-modal-toggle={brand._id} className="w-[100%] sm:w-1/2 md:w-1/3 lg:w-1/4 p-3    font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-      <Item key={brand._id} brand={brand}/>
-      <h1>{brand._id}</h1>
-    </button>
+  {brands.length==0 && <Loader/>}
+  {brands.length !=0 &&  <>
+  {brands.map(brand=><React.Fragment key={brand._id} ><button onClick={()=>setModalOpen(brand)} data-modal-target={brand._id} data-modal-toggle={brand._id} className="w-[100%] sm:w-1/2 md:w-1/3 lg:w-1/4 p-3    font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+      <Item  brand={brand}/> {console.log(brand._id)}
+    </button></React.Fragment>)}
     {/* Main modal */}
-    <div id={brand._id} tabIndex={-1} aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    {ModalOpen?<div id={ModalOpen._id} tabIndex={-1} aria-hidden="true" className=" overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full flex ">
       <div className="relative p-4 w-full max-w-2xl max-h-full">
         {/* Modal content */}
         <div className="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
           {/* Modal header */}
           <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
-            <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide={brand._id}>
+            <button onClick={()=>setModalOpen(null)} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide={ModalOpen._id}>
               <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
               </svg>
@@ -65,19 +64,25 @@ export default function Brands() {
           </div>
           {/* Modal body */}
           <div className="p-4 md:p-5 space-y-4">
-              <Item key={brand._id} brand={brand}/> 
+            <div className='flex  '>
+                <div className='basis-[50%] flex flex-col justify-center '>
+                  <h3 className='text-3xl text-main font-medium'>{ModalOpen.name}</h3>
+                  <h4 className='text-lg'>{ModalOpen.slug}</h4>
+                </div>
+                <div className='basis-[50%]'>
+                  <Item  brand={ModalOpen}/> 
+                </div>
+            </div>
           </div>
           {/* Modal footer */}
           <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-            <button data-modal-hide={brand._id} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Close</button>
+            <button onClick={()=>setModalOpen(null)} data-modal-hide={ModalOpen._id} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Close</button>
           </div>
         </div>
       </div>
-    </div>
-    </>}
-  )}
-</div>
+    </div>:""}
+  </> }
+ 
 
-    </>
-  )
-}
+</div>
+</>)}
---
+++ src/components/Categories/components/SubCategories/SubCategories.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import styles from './SubCategories.module.css'
+import axios from 'axios'
+export default function SubCategories(props) {
+    const[count,setCount]=useState(0)
+    const[SubCategory,setSubCategory]=useState([])
+    let{name,_id}= props.cate
+    console.log(name , _id)
+    async function getsubCategories(){
+      try{
+        let{data}=await axios.get(`https://ecommerce.routemisr.com/api/v1/subcategories`)
+        console.log(data.data)
+        console.log(data.data.category)
+        let res =data.data.filter(ele =>ele.category == _id)
+        console.log(res)
+        setSubCategory(res)
+      }catch(error){
+        console.log(error.response.data.message)
+      }
+    }
+    useEffect(()=>{
+      getsubCategories()
+    },[_id])
+  return (
+    <>
+    {_id && SubCategory.length!==0 &&<div>
+      <h3 className='text-center text-main text-3xl font-semibold'>{`${name} subcategories`}</h3>
+      <div className='flex flex-wrap py-10'>
+        {SubCategory.map(sub=><div className='box w-[100%] sm:w-1/2 md:w-1/3  border-2 py-3 px-5 mb-2 mx-3 sm:mx-0'>
+          <h4 className='text-center font-bold'>{sub.name}</h4>
+        </div>)}
+      </div>
+    </div>}
+    </>
+    
+  )
+}
